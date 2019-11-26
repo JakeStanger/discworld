2abf55e9f1e3d95464da4a94d11845fefd61ebd5
Jake Stanger <mail@jstanger.dev>
2019-11-26 23:36:45 +0000

    use object, add ability to cull

--- client/entity/Cube.ts
+++ client/entity/Cube.ts
@@ -1,36 +1,55 @@
 import {shadeColor} from "../utils";
 
-export const cube = (ctx: CanvasRenderingContext2D, x: number, y: number, size: number, color: string) => {
-  ctx.beginPath();
-  ctx.moveTo(x, y + size);
-  ctx.lineTo(x + size, y);
-  ctx.lineTo(x + size, y - size);
-  ctx.lineTo(x, y);
-  ctx.closePath();
-  ctx.fillStyle = shadeColor(color, -10);
-  ctx.strokeStyle = color;
-  ctx.stroke();
-  ctx.fill();
+export interface ICube {
+  ctx: CanvasRenderingContext2D,
+  x: number,
+  y: number,
+  size: number,
+  color: string,
+  drawLeft?: boolean,
+  drawFront?: boolean,
+  drawTop?: boolean
+}
 
-  ctx.beginPath();
-  ctx.moveTo(x, y + size);
-  ctx.lineTo(x + size, y + size);
-  ctx.lineTo(x + size * 2, y);
-  ctx.lineTo(x + size, y);
-  ctx.closePath();
-  ctx.fillStyle = shadeColor(color, 10);
-  ctx.strokeStyle = shadeColor(color, 50);
-  ctx.stroke();
-  ctx.fill();
+export const cube = (options: ICube) => {
+  const {ctx, x, y, size, color, drawLeft, drawFront, drawTop} = options;
 
-  ctx.beginPath();
-  ctx.moveTo(x + size, y);
-  ctx.lineTo(x + size * 2, y);
-  ctx.lineTo(x + size * 2, y - size);
-  ctx.lineTo(x + size, y - size);
-  ctx.closePath();
-  ctx.fillStyle = shadeColor(color, 20);
-  ctx.strokeStyle = shadeColor(color, 60);
-  ctx.stroke();
-  ctx.fill();
+  if (drawLeft !== false) {
+    ctx.beginPath();
+    ctx.moveTo(x, y + size);
+    ctx.lineTo(x + size, y);
+    ctx.lineTo(x + size, y - size);
+    ctx.lineTo(x, y);
+    ctx.closePath();
+    ctx.fillStyle = shadeColor(color, -10);
+    ctx.strokeStyle = color;
+    ctx.stroke();
+    ctx.fill();
+  }
+
+  if (drawFront !== false) {
+    ctx.beginPath();
+    ctx.moveTo(x, y + size);
+    ctx.lineTo(x + size, y + size);
+    ctx.lineTo(x + size * 2, y);
+    ctx.lineTo(x + size, y);
+    ctx.closePath();
+    ctx.fillStyle = shadeColor(color, 10);
+    ctx.strokeStyle = shadeColor(color, 50);
+    ctx.stroke();
+    ctx.fill();
+  }
+
+  if (drawTop !== false) {
+    ctx.beginPath();
+    ctx.moveTo(x + size, y);
+    ctx.lineTo(x + size * 2, y);
+    ctx.lineTo(x + size * 2, y - size);
+    ctx.lineTo(x + size, y - size);
+    ctx.closePath();
+    ctx.fillStyle = shadeColor(color, 20);
+    ctx.strokeStyle = shadeColor(color, 60);
+    ctx.stroke();
+    ctx.fill();
+  }
 };
