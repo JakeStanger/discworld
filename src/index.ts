8eddb9cbc5ee2156cff609b95a785b96efea8576
Jake Stanger <mail@jstanger.dev>
2019-11-28 23:25:22 +0000

    feat(server): load markov chains and websockets on startup

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,8 @@ import * as commands from "./commands";
 import IDatabaseConfig from "./database/IDatabaseConfig";
 import { writeMessage } from "./commands/sync";
 import WebServer from "./server";
+import Markov from "./utils/Markov";
+import Webhooks from "./utils/Webhooks";
 
 
 client.on('ready', () => {
@@ -16,6 +18,7 @@ client.on('ready', () => {
 
 client.on('message', async msg => {
     const content = msg.content;
+
     if(content.startsWith(config.prefix)) {
         const command = content.substr(1).split(" ")[0];
         if(command in commands) {
@@ -32,8 +35,17 @@ async function init() {
   await database.setup(config.database as IDatabaseConfig);
 
   await client.login(config.token);
+  await client.user.setStatus("dnd");
+  await client.user.setActivity("Loading");
 }
 
-init().then(() => {
+init().then(async () => {
   new WebServer(client).listen();
+
+  await new Markov(client).load();
+
+  await new Webhooks(client).load();
+
+  await client.user.setStatus("online");
+  await client.user.setActivity("you", {type: "WATCHING"});
 });
